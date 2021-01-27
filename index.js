8ae56b39a47f53e39772160ca52f6d5865525f45
xueyiyao <alleny111@gmail.com>
2021-01-27 03:22:38 -0800

    images and videos enlarge when clicked

--- index.js
+++ index.js
@@ -26,3 +26,39 @@ function emailSubmit() {
     document.getElementById("email-success").style.display = "none";
   }
 }
+
+function popupOn(src) {
+  document.getElementById("popup-overlay").style.display = "block";
+  console.log(src);
+  document.getElementById("popup-img").src = src;
+}
+
+function popupOff() {
+  document.getElementById("popup-overlay").style.display = "none";
+  document.getElementById("popup-overlay-video").style.display = "none";
+  document.getElementById("popup-img").src = "";
+  document.getElementById("popup-video").src = "";
+}
+
+function popupVideo(id) {
+  document.getElementById("popup-overlay-video").style.display = "block";
+  let src;
+  switch (id) {
+    case "video-overlay-1":
+      src = "https://www.youtube.com/embed/S77A_7w7LYk";
+      break;
+    case "video-overlay-2":
+      src = "https://www.youtube.com/embed/KMc3vL_MIeo";
+      break;
+    case "video-overlay-3":
+      src = "https://www.youtube.com/embed/8qRZcXIODNU";
+      break;
+    case "video-overlay-4":
+      src = "https://www.youtube.com/embed/-QlxCQThAI8";
+      break;
+    case "video-overlay-5":
+      src = "https://www.youtube.com/embed/H1KP4ztKK0A";
+      break;
+  }
+  document.getElementById("popup-video").src = src;
+}
